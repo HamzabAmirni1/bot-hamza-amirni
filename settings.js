44c0d490db14fcdfefd01f0efd92e4f1a3a87bf3
Hamza Amirni <hamzaamirni451@gmail.com>
2026-06-20 16:24:02 +0100

    chore: update Supabase credentials to new project and secret API key

--- settings.js
+++ settings.js
@@ -42,8 +42,8 @@ const settings = {
   hfToken: '', // HuggingFace Token for Qwen AI
 
   // Supabase, Facebook & Telegram configs
-  supabaseUrl: process.env.SUPABASE_URL || 'https://xmmthiitoezusoejydta.supabase.co',
-  supabaseKey: process.env.SUPABASE_KEY || 'sb_publishable_obLwMpkUXz2zDnGKKK9bWA_HV9SE9k_',
+  supabaseUrl: process.env.SUPABASE_URL || 'https://uadsxbfoqqsclubsorvf.supabase.co',
+  supabaseKey: process.env.SUPABASE_KEY || 'sb_secret_kvnaI88hIuqV9_KqwthkqA_rYdE-Wz3',
   telegramToken: process.env.TELEGRAM_TOKEN || '8589218915:AAFoh4mnEsnuQOjZjgDrcSTQus7ClnL2VTA',
   fbPageAccessToken: process.env.PAGE_ACCESS_TOKEN || 'EAARU3lwIKlcBQz4GqbCw2Vc6ZAAPKytsEfhN6nCZBbXHdIRQZCchkjUq9BB5k622kDDRQaZCgBRB4pTCRN30hG25QPTZCYvyoYRsZB7MlBpHyHjb9ZAbbnZCkNAEmMFXZB35zCG2xCUjpNVQhWFP00KmTwNP1MryAeRgZBkRbMOZCSaGv6o0zP5XRWEq15cB6gYk6PbwT2BiQZDZD',
   fbPageId: process.env.FB_PAGE_ID || 'me',
